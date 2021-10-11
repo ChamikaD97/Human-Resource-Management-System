54d2c19d29a4acadab495598cc5fb4e7b1e9949d
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2021-10-11 09:41:28 +0530

    Update attendanceBean.java

--- src/main/java/attendance/attendanceBean.java
+++ src/main/java/attendance/attendanceBean.java
@@ -4,7 +4,6 @@ public class attendanceBean {
 
     private String empId,  workedHoursFrom,workedHoursTo,otHoursFrom,otHoursTo,from,to,attendanceId,date,attendTime;
     
-    
     private float otHours;
     private String workedHr,leaveTime;
 
